f9043e856f1835511fed20d666e7da696401829f
liran.redl <liran.redl66666@gmail.com>
2022-11-27 19:56:29 +0200

    Updated 27.11.22 13:30 yalla macabi

--- WorldCup.cpp
+++ WorldCup.cpp
@@ -1,5 +1,22 @@
 #include "WorldCup.h"
+
 WorldCup::WorldCup() {
+    top_player_score = 0;
+    top_player = nullptr;
+    legal_teams_by_id = AvlTree<int, Team*>();
+    teams_by_id = AvlTree<int, Team*>();
+    all_players_by_id = AvlTree<int, Player*>();
+    all_players_by_score = AvlTree<Score, Player*>();
+}
+
+WorldCup::~WorldCup() {
 
 }
+
+StatusType WorldCup::AddTeam(int teamID, int points) {
+
+}
+
+
+
 
